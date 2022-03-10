cce36e78d89a3c3d88ca5e8600b4a24b96e4e539
Akash <akashpatel1205@gmail.com>
2022-03-09 19:36:10 -0500

    Lab finished extension needed

--- src/BingoCard.java
+++ src/BingoCard.java
@@ -1,35 +1,43 @@
 //Akash Patel   ITCS
 
 public class BingoCard {
-    public String[][] card;
+    String[][] card = new String[6][5];
 
     public BingoCard() {
         generateCard();
     }
 
     public void generateCard() {
-        card = new String[6][5];
+
+        for (int row = 0; row < card.length; row++) {
+            for (int col = 0; col < card[row].length; col++) {
+                if (row == 3 && col == 2) {
+                    card[row][col] = "F";
+                } else {
+                    card[row][col] = (int) (Math.random() * (15) + col * 15 + 1) + "";
+                }
+            }
+            System.out.println();
+        }
 
         card[0][0] = "B";
         card[0][1] = "I";
         card[0][2] = "N";
         card[0][3] = "G";
         card[0][4] = "O";
-
-        for (int row = 0; row < card.length; row++) {
-            for (int col = 0; col < card[0].length; col++) {
-                card[row][col] = (int) (Math.random() * 1) + "\t";
-            }
-            System.out.println();
-        }
     }
 
     public void displayCard() {
         for (int row = 0; row < card.length; row++) {
-            for (int col = 0; col < card[0].length; col++) {
+            for (int col = 0; col < card[row].length; col++) {
                 System.out.print(card[row][col] + "\t");
             }
-            System.out.println();
+            System.out.println("");
         }
+    }
+
+    public static void main(String[] args) {
+        BingoCard card = new BingoCard();
+        card.displayCard();
     }
 }
