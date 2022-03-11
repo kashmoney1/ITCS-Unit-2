5f7a5bb3cee8d5058b95f498147daa9907ffccb3
Akash Patel <akashpatel1205@gmail.com>
2022-03-11 10:58:44 -0500

    Deleted tester

--- src/BingoCardTester.java
+++
@@ -1,8 +0,0 @@
-//Akash Patel   ITCS
-
-public class BingoCardTester {
-    public static void main(String[] args) {
-        BingoCard card = new BingoCard();
-        card.displayCard();
-    }
-}
