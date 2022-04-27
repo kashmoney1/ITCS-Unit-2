8f4b54cb9d96c465d5fe0bbb2939e76869ea32e2
881057 <akashpatel1205@gmail.com>
2022-04-27 16:48:04 -0400

    tic tac toe bot started

--- src/YogeshSlayer.java
+++ src/YogeshSlayer.java
@@ -25,7 +25,7 @@ public class YogeshSlayer extends TicTacToePlayer {
                     move[0] = 0;
                     move[1] = 2;
                 }
-                //if in the upper or lower left hand corner move to the upper right corner
+                //if in the upper or lower left corner move to the upper right corner
                 else if (board[0][0] == 2 || board[2][0] == 2) {
                     move[0] = 0;
                     move[1] = 2;
@@ -42,14 +42,76 @@ public class YogeshSlayer extends TicTacToePlayer {
                 }
             }
             else if (GameController.getTurnCount() == 4) {
-                //if O isn't at [1][2]
-                if (board[1][2] != 2) {
-                    move[0] = 1;
-                    move[1] = 2;
+                //if placed on center for second move
+                if (board[1][1] == 2 && board[0][2] == 1) {
+                    //if placed on any edge except [1][2]
+                    if (board[1][0] == 2 || board[0][1] == 2 || board[2][1] == 2) {
+                        move[0] = 1;
+                        move[1] = 2;
+                    }
+                    //if placed on [1][2]
+                    else if (board[1][2] == 2) {
+                        move[0] = 1;
+                        move[1] = 0;
+                    }
+                    //if placed on upper or lower left corner
+                    else if (board[0][0] == 2 || board[2][0] == 2) {
+                        move[0] = 1;
+                        move[1] = 2;
+                    }
+                }
+                //if in the upper or lower left corner for second move
+                else if (board[0][0] == 2 || board[2][0] == 2 && board[0][2] == 1) {
+                    //if it moves on any edge except [1][2]
+                    if (board[1][0] == 2 || board[0][1] == 2 || board[2][1] == 2) {
+                        move[0] = 1;
+                        move[1] = 2;
+                    }
+                    //if placed on [1][2]
+                    else if (board[1][2] == 2) {
+                        move[0] = 2;
+                        move[1] = 0;
+                        //if [2][0] is taken
+                        if (board[2][0] == 2) {
+                            move[0] = 0;
+                            move[1] = 0;
+                        }
+                    }
+                    else {
+                        move[0] = 1;
+                        move[1] = 2;
+                    }
+                }
+                //if the second move was to the upper right corner
+                else if (board[0][2] == 2) {
+                    //if [2][1] is empty then move there to win
+                    if (board[2][1] == 0) {
+                        move[0] = 2;
+                        move[1] = 1;
+                    }
+                    else {
+                        move[0] = 0;
+                        move[1] = 0;
+                    }
                 }
             }
             else if (GameController.getTurnCount() == 6) {
-
+                //if placed on center for second move
+                if (board[1][1] == 2 && board[0][2] == 1) {
+                    //if placed on [1][2]
+                    if (board[1][2] == 2) {
+                        //if placed on upper left corner
+                        if (board[0][0] == 2) {
+                            move[0] = 0;
+                            move[1] = 1;
+                        }
+                        //if placed on lower left corner
+                        else if (board[2][0] == 2) {
+                            move[0] = 2;
+                            move[1] = 1;
+                        }
+                    }
+                }
             }
 
         }
