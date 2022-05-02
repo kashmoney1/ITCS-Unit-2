a34fc23843a2d4d03d5b278ad5b43e321c38a25a
Akash <akashpatel1205@gmail.com>
2022-05-02 11:34:05 -0400

    file practice printFile1 and 2 methods complete

--- src/FilePractice.java
+++ src/FilePractice.java
@@ -3,24 +3,50 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class FilePractice {
-    public static void printFile() {
-        File myFile = new File("FileTest\\filetest1.txt");
-        Scanner s = null;
+    public static int printFile() {
+        File myFile = new File("src\\filetest1.txt");
+        Scanner s;
+        int sum = 0;
 
         try {
             s = new Scanner(myFile);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
-            return;
+            return 0;
         }
 
         while (s.hasNextLine()) {
-            System.out.println(s.nextLine());
+            try {
+                sum += Integer.parseInt(s.nextLine());
+            } catch (Exception ignored) {
+            }
         }
-        System.out.println("End of File");
-        s.close();
+        return sum;
     }
+
+    public static int printFile2() {
+        File mySecondFile = new File("src\\filetest2.txt");
+        Scanner s;
+        int sum = 0;
+
+        try {
+            s = new Scanner(mySecondFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return 0;
+        }
+
+        while(s.hasNextLine()) {
+            try {
+                sum += Integer.parseInt(s.nextLine());
+            } catch (Exception ignored) {
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        printFile();
+        System.out.println(printFile());
+        System.out.println(printFile2());
     }
 }
